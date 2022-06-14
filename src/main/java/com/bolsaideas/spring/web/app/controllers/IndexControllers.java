f6e7f058d9e3de1e221129899c48993354b22378
gonzalo-hg <gonzalo.hg19@gmail.com>
2022-06-14 00:48:04 -0500

    Segundo avance

--- src/main/java/com/bolsaideas/spring/web/app/controllers/IndexControllers.java
+++ src/main/java/com/bolsaideas/spring/web/app/controllers/IndexControllers.java
@@ -1,9 +1,17 @@
 package com.bolsaideas.spring.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bolsaideas.spring.web.app.model.Usuario;
 
 @Controller//Componente de Srping como controlador
 public class IndexControllers {
@@ -20,4 +28,38 @@ public class IndexControllers {
 		return "index";
 	}
 
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Gonzalo");
+		usuario.setApellido("Hernandez");
+		usuario.setEmail("gonzalo@gmail.com");
+		
+		model.addAttribute("usuario",usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		
+		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		
+		
+		
+		
+		model.addAttribute("titulo", "Listado de  usaurios:");
+
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Gonzalo","Gonzalez","zalo@gmail.com"),
+				new Usuario("Gonzalo1","Gonzalez","zalo@gmail.com"),
+				new Usuario("Gonzalo2","Gonzalez","zalo@gmail.com"),
+				new Usuario("Gonzalo3","Gonzalez","zalo@gmail.com"));
+		
+		return usuarios;
+		
+	}
 }
